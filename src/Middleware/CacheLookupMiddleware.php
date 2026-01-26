13c018eb1f618841f5a8e97516b813a7c47df3a3
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-01-26 13:03:25 +0200

    refactor: Update CacheLookupMiddleware to use native ReactPHP promise resolution

--- src/Middleware/CacheLookupMiddleware.php
+++ src/Middleware/CacheLookupMiddleware.php
@@ -9,10 +9,10 @@ use Fyennyi\AsyncCache\Event\CacheHitEvent;
 use Fyennyi\AsyncCache\Event\CacheStatusEvent;
 use Fyennyi\AsyncCache\Model\CachedItem;
 use Fyennyi\AsyncCache\Storage\CacheStorage;
-use GuzzleHttp\Promise\Create;
-use GuzzleHttp\Promise\PromiseInterface;
 use Psr\EventDispatcher\EventDispatcherInterface;
 use Psr\Log\LoggerInterface;
+use React\Promise\PromiseInterface;
+use function React\Promise\resolve;
 
 /**
  * Handles initial cache lookup, freshness check and X-Fetch logic
@@ -39,7 +39,6 @@ class CacheLookupMiddleware implements MiddlewareInterface
             $context->staleItem = $cached_item;
             $is_fresh = $cached_item->isFresh();
 
-            // X-Fetch logic
             if ($is_fresh && $context->options->x_fetch_beta > 0 && $cached_item->generationTime > 0) {
                 $rand = mt_rand(1, mt_getrandmax()) / mt_getrandmax();
                 $check = time() - ($cached_item->generationTime * $context->options->x_fetch_beta * log($rand));
@@ -53,17 +52,16 @@ class CacheLookupMiddleware implements MiddlewareInterface
             if ($is_fresh) {
                 $this->dispatcher?->dispatch(new CacheStatusEvent($context->key, CacheStatus::Hit, microtime(true) - $context->startTime, $context->options->tags));
                 $this->dispatcher?->dispatch(new CacheHitEvent($context->key, $cached_item->data));
-                return Create::promiseFor($cached_item->data);
+                return resolve($cached_item->data);
             }
 
             if ($context->options->strategy === CacheStrategy::Background) {
                 $this->dispatcher?->dispatch(new CacheStatusEvent($context->key, CacheStatus::Stale, microtime(true) - $context->startTime, $context->options->tags));
                 $this->dispatcher?->dispatch(new CacheHitEvent($context->key, $cached_item->data));
                 
-                // Trigger background refresh (we don't wait for it)
                 $next($context);
                 
-                return Create::promiseFor($cached_item->data);
+                return resolve($cached_item->data);
             }
         }
 
