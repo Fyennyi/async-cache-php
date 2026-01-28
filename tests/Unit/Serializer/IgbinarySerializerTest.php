ed3675a49c5814e444fe7acb1b4521a8ba27c3ee
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-01-29 01:51:29 +0200

    test: Add unit tests for igbinary extension missing scenario

--- tests/Unit/Serializer/IgbinarySerializerTest.php
+++ tests/Unit/Serializer/IgbinarySerializerTest.php
@@ -1,5 +1,29 @@
 <?php
 
+namespace Fyennyi\AsyncCache\Serializer;
+
+/**
+ * Shadowing extension_loaded to simulate missing igbinary.
+ */
+function extension_loaded($name)
+{
+    if ('igbinary' === $name && !empty($GLOBALS['mock_igbinary_missing'])) {
+        return false;
+    }
+    return \extension_loaded($name);
+}
+
+/**
+ * Shadowing function_exists to simulate missing igbinary functions.
+ */
+function function_exists($name)
+{
+    if (str_contains($name, 'igbinary') && !empty($GLOBALS['mock_igbinary_missing'])) {
+        return false;
+    }
+    return \function_exists($name);
+}
+
 namespace Tests\Unit\Serializer;
 
 use Fyennyi\AsyncCache\Serializer\IgbinarySerializer;
@@ -7,15 +31,53 @@ use PHPUnit\Framework\TestCase;
 
 class IgbinarySerializerTest extends TestCase
 {
-    protected function setUp() : void
+    protected function tearDown() : void
     {
-        if (!extension_loaded('igbinary')) {
-            $this->markTestSkipped('igbinary extension is not loaded');
-        }
+        $GLOBALS['mock_igbinary_missing'] = false;
+    }
+
+    public function testThrowsExceptionIfExtensionNotLoaded() : void
+    {
+        $GLOBALS['mock_igbinary_missing'] = true;
+
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('igbinary extension is not loaded');
+
+        new IgbinarySerializer();
+    }
+
+    public function testSerializeThrowsIfFunctionMissing() : void
+    {
+        $GLOBALS['mock_igbinary_missing'] = true;
+
+        $reflection = new \ReflectionClass(IgbinarySerializer::class);
+        $serializer = $reflection->newInstanceWithoutConstructor();
+
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('igbinary extension is not loaded');
+
+        $serializer->serialize(['data']);
+    }
+
+    public function testUnserializeThrowsIfFunctionMissing() : void
+    {
+        $GLOBALS['mock_igbinary_missing'] = true;
+
+        $reflection = new \ReflectionClass(IgbinarySerializer::class);
+        $serializer = $reflection->newInstanceWithoutConstructor();
+
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('igbinary extension is not loaded');
+
+        $serializer->unserialize('data');
     }
 
     public function testSerializeUnserialize() : void
     {
+        if (!\extension_loaded('igbinary')) {
+            $this->markTestSkipped('igbinary extension is not loaded');
+        }
+
         $serializer = new IgbinarySerializer();
         $data = ['a' => 'b', 'c' => 1];
 
