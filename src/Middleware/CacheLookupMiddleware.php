7ed048cdb4eae888bfa1d664aeeead72ed899232
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-02-28 11:23:02 +0200

    refactor: Remove strategy logic from CacheLookupMiddleware and delegate to StrategyMiddleware

--- src/Middleware/CacheLookupMiddleware.php
+++ src/Middleware/CacheLookupMiddleware.php
@@ -62,79 +62,63 @@ class CacheLookupMiddleware implements MiddlewareInterface
      */
     public function handle(CacheContext $context, callable $next) : PromiseInterface
     {
-        // Basic tracing for debugging
-        $this->logger->debug('CacheLookupMiddleware: handling cache context', ['key' => $context->key, 'strategy' => $context->options->strategy->value]);
+        $this->logger->debug('LOOKUP_START: Beginning cache retrieval', ['key' => $context->key, 'strategy' => $context->options->strategy->value]);
+
         if (CacheStrategy::ForceRefresh === $context->options->strategy) {
+            $this->logger->debug('LOOKUP_BYPASS: Strategy is ForceRefresh, bypassing cache', ['key' => $context->key]);
             $this->dispatcher?->dispatch(new CacheStatusEvent($context->key, CacheStatus::Bypass, 0, $context->options->tags, (float) $context->clock->now()->format('U.u')));
 
             return $next($context);
         }
 
-        /** @var PromiseInterface<T> $promise */
-        $promise = $this->storage->get($context->key, $context->options)->then(
+        return $this->storage->get($context->key, $context->options)->then(
             function ($cached_item) use ($context, $next) {
-                if ($cached_item instanceof CachedItem) {
-                    $context->stale_item = $cached_item;
-                    $now_ts = $context->clock->now()->getTimestamp();
-                    $is_fresh = $cached_item->isFresh($now_ts);
+                if (! ($cached_item instanceof CachedItem)) {
+                    $this->logger->debug('LOOKUP_MISS: Item not found in cache', ['key' => $context->key]);
+                    $this->dispatcher?->dispatch(new CacheMissEvent($context->key, (float) $context->clock->now()->format('U.u')));
 
-                    if ($is_fresh && $context->options->x_fetch_beta > 0 && $cached_item->generation_time > 0) {
-                        $rand = mt_rand(1, mt_getrandmax()) / mt_getrandmax();
-                        $check = $now_ts - ($cached_item->generation_time * $context->options->x_fetch_beta * log($rand));
+                    return $next($context);
+                }
 
-                        if ($check > $cached_item->logical_expire_time) {
-                            $now = (float) $context->clock->now()->format('U.u');
-                            $this->dispatcher?->dispatch(new CacheStatusEvent($context->key, CacheStatus::XFetch, $context->getElapsedTime(), $context->options->tags, $now));
-                            $is_fresh = false;
-                        }
-                    }
+                $context->stale_item = $cached_item;
+                $now_ts = $context->clock->now()->getTimestamp();
+                $is_fresh = $cached_item->isFresh($now_ts);
 
-                    if ($is_fresh) {
+                if ($is_fresh && $context->options->x_fetch_beta > 0 && $cached_item->generation_time > 0) {
+                    $rand = mt_rand(1, mt_getrandmax()) / mt_getrandmax();
+                    $check = $now_ts - ($cached_item->generation_time * $context->options->x_fetch_beta * log($rand));
+
+                    if ($check > $cached_item->logical_expire_time) {
+                        $this->logger->debug('LOOKUP_XFETCH: Probabilistic early expiration triggered', ['key' => $context->key]);
                         $now = (float) $context->clock->now()->format('U.u');
-                        $this->dispatcher?->dispatch(new CacheStatusEvent($context->key, CacheStatus::Hit, $context->getElapsedTime(), $context->options->tags, $now));
-                        $this->dispatcher?->dispatch(new CacheHitEvent($context->key, $cached_item->data, $now));
+                        $this->dispatcher?->dispatch(new CacheStatusEvent($context->key, CacheStatus::XFetch, $context->getElapsedTime(), $context->options->tags, $now));
 
-                        // If item has tags, we MUST continue to TagValidationMiddleware
-                        if (! empty($cached_item->tag_versions)) {
-                            return $next($context);
-                        }
-
-                        /** @var T $item_data */
-                        $item_data = $cached_item->data;
-
-                        return \React\Promise\resolve($item_data);
+                        // Create a stale version of the item by setting logical_expire_time to current time - 1
+                        $context->stale_item = new CachedItem(
+                            $cached_item->data,
+                            $now_ts - 1,
+                            $cached_item->version,
+                            $cached_item->is_compressed,
+                            $cached_item->generation_time,
+                            $cached_item->tag_versions
+                        );
+                        $is_fresh = false;
                     }
+                }
 
-                    if (CacheStrategy::Background === $context->options->strategy) {
-                        $now = (float) $context->clock->now()->format('U.u');
-                        $this->dispatcher?->dispatch(new CacheStatusEvent($context->key, CacheStatus::Stale, $context->getElapsedTime(), $context->options->tags, $now));
-                        $this->dispatcher?->dispatch(new CacheHitEvent($context->key, $cached_item->data, $now));
-
-                        // Background fetch - catch errors to prevent unhandled rejection since this promise is not returned
-                        $next($context)->catch(function (\Throwable $e) use ($context) {
-                            $this->logger->error('AsyncCache BACKGROUND_FETCH_ERROR: {msg}', ['key' => $context->key, 'msg' => $e->getMessage()]);
-                        });
-
-                        /** @var T $item_data */
-                        $item_data = $cached_item->data;
-
-                        return \React\Promise\resolve($item_data);
-                    }
+                if ($is_fresh) {
+                    $this->logger->debug('LOOKUP_FRESH: Fresh item found in cache', ['key' => $context->key]);
+                } else {
+                    $this->logger->debug('LOOKUP_STALE: Stale item found in cache', ['key' => $context->key]);
                 }
 
                 return $next($context);
             },
             function (\Throwable $e) use ($context, $next) {
-                $this->logger->error('AsyncCache CACHE_LOOKUP_ERROR: {msg}', ['key' => $context->key, 'msg' => $e->getMessage()]);
+                $this->logger->error('LOOKUP_ERROR: Cache retrieval failed', ['key' => $context->key, 'error' => $e->getMessage()]);
 
                 return $next($context);
             }
         );
-
-        $promise->catch(function (\Throwable $e) use ($context) {
-            $this->logger->debug('AsyncCache LOOKUP_PIPELINE_ERROR: {msg}', ['key' => $context->key, 'msg' => $e->getMessage()]);
-        });
-
-        return $promise;
     }
 }
